be405058acd15cb4dc7673e37b25e9f9926c7e9e
Varad Muley <varad.muley408@gmail.com>
2026-03-12 23:47:06 +0530

    Update program15.js

---
+++ program15.js
@@ -0,0 +1,18 @@
+
+function main()
+{
+    let No1 = 10, No2 = 15;
+
+    if(No1 > No2)
+    {
+        console.log("No1 is max");
+    }
+    else
+    {
+        console.log("No2 is max");
+    }
+}
+
+main();
+
+
