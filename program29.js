446b03267e60de97b962a19b6cb5334bedd8abf4
Varad Muley <varad.muley408@gmail.com>
2026-03-13 23:47:15 +0530

    Update program29.js

---
+++ program29.js
@@ -0,0 +1,29 @@
+function CountDigits(No)
+{
+    let iCount = 0;
+    let iDigit = 0 ;
+
+    while(No != 0)
+    {
+        iDigit = No % 10;
+        iCount++;
+        No = Math.floor(No/10); 
+        //No = parseInt(No/10);   he pan chaltaa 
+    }
+    return iCount;
+    
+}
+
+function main()
+{
+    let iValue = 721;
+    let iRet = 0 ;
+
+    iRet = CountDigits(iValue);
+
+    console.log(`${iValue} contains ${iRet} digits`);
+}
+
+main();
+
+
