34eece559885e5799296a114d6963bac342bdd7e
Varad Muley <varad.muley408@gmail.com>
2026-03-17 22:57:17 +0530

    Update program41.js

---
+++ program41.js
@@ -0,0 +1,13 @@
+
+function main()
+{
+   let obj = {name : "Varad", salary : 37 };   // JSON
+
+   //console.log(obj[0]); Not Allowed
+   console.log(obj.name);
+   console.log(obj.salary);
+
+   //console.log(obj[name]); Not Allowed
+}
+
+main();
