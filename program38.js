56ca711d5b1c5c21b0434135827a212c43149b2e
Varad Muley <varad.muley408@gmail.com>
2026-03-17 20:53:56 +0530

    Update program38.js

---
+++ program38.js
@@ -0,0 +1,18 @@
+
+function main()
+{
+   const Arr = [10,20,30,40];  // change
+
+   console.log(Arr[0]);
+   console.log(Arr[1]);
+   console.log(Arr[2]);
+   console.log(Arr[3]);
+
+   Arr[0] = 11;
+
+   console.log(Arr[0]);
+
+
+}
+
+main();
