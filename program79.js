a1c80e7ea257714b620d1e3f6f47610f6f0ad199
Varad Muley <varad.muley408@gmail.com>
2026-03-18 10:02:23 +0530

    Update program79.js

---
+++ program79.js
@@ -0,0 +1,14 @@
+function StringLength(str)
+{
+    return str.length;
+}
+
+function main()
+{
+    let str = "Hello JavaScript";
+
+    let result = StringLength(str);
+    console.log("Length:", result);
+}
+
+main();
