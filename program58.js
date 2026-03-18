c07dec74bb0ebd0cdc9e01aca5e0aa51ce55c760
Varad Muley <varad.muley408@gmail.com>
2026-03-18 09:47:38 +0530

    Update program58.js

---
+++ program58.js
@@ -0,0 +1,30 @@
+function count (str)
+{
+    let iCnt = 0 ;
+    let iCount = 0 ;
+    
+    for(iCnt = 0 ; iCnt < str.length; iCnt++)
+    {
+        if((str.charAt(iCnt) >= 'a') && (str.charAt(iCnt) <= 'z') );
+        {
+            iCount++;
+        }
+    }   
+    
+    console.log(iCount);
+}
+function main()
+{
+    let str = "Hello";
+    let iRet = 0;
+
+    iRet = count(str);
+
+    console.log(iRet);
+
+    
+}
+
+main();
+
+
