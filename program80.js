418f4c3a82f4337f48875779cae42bc86df3d8e7
Varad Muley <varad.muley408@gmail.com>
2026-03-18 10:02:37 +0530

    Update program80.js

---
+++ program80.js
@@ -0,0 +1,18 @@
+function Display(arr)
+{
+    let i = 0;
+
+    for(i = 0; i < arr.length; i++)
+    {
+        console.log(arr[i]);
+    }
+}
+
+function main()
+{
+    let arr = [5, 10, 15, 20];
+
+    Display(arr);
+}
+
+main();
